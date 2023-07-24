0eceb52ee7cb85153c75fc55c52199d23a7e1815
khayru <43963328+khayru@users.noreply.github.com>
2023-07-24 16:15:39 -0400

    new project 3 added

---
+++ app.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3ad8f68b6a2f5b0d6a8d369245f5503d3c56990c550d2bc9001c10af0848fe2a
+size 2119
---
+++ static/js/car.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3c239b3c557893bda8d02fc20e2546be9a63ad5dd1e046c400d775efd9a5dac0
+size 959
