fc2456a4ae13938af06d4f013a4237b61fbba169
khayru <43963328+khayru@users.noreply.github.com>
2023-07-25 11:19:22 -0400

    first commit

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3ad8f68b6a2f5b0d6a8d369245f5503d3c56990c550d2bc9001c10af0848fe2a
-size 2119
+oid sha256:51ce482080f90e4c709a2afd0272d5402ddcdea488715d23c9bc9fe6b2660195
+size 3230
---
+++ templates/static/js/car.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:136fc0cc1702fdbf3439893c02b9a97c0f2e7aefd6d1eb32397b033650f9b04c
+size 1038
